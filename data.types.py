aec19b765a19a96f7dd6a3c0900fa26f04fe8a37
masalakie <masalakiebangura27@gmail.com>
2024-07-27 18:02:32 -0400

    Create data.types.py

---
+++ data.types.py
@@ -0,0 +1,26 @@
+var = "test"
+
+print(type(var))
+
+var2 = 493
+
+print(type(var2))
+
+var3 = '649'
+
+print(type(var3))
+
+var4 = "can't"
+var5 = '"Get rich or die trying"'
+var6 = '"can\'t means won\'t"'
+var7 = '"keep grinding"'
+
+
+print(var4)
+print(var5)
+print(var6)
+print(var7)
+
+
+var8 = int(var3)
+
